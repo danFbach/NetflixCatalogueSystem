3f4c97c873c98d416d49f332c803a9e8ba6960a9
danFbach <quiktekrepair@gmail.com>
2016-04-06 18:13:15 -0500

    started some code, nothing special yet

--- NetflixCatalogueSystem/genre.cs
+++ NetflixCatalogueSystem/genre.cs
@@ -8,5 +8,23 @@ namespace NetflixCatalogueSystem
 {
     public class genre
     {
+        List<title> titles = new List<title>();
+
+        public void addNewTitles()
+        {
+            foreach(action movie in action)
+            {
+                titles.Add(new action());
+            }
+        }
+
+        public void titleIterator()
+        {
+            foreach(title movie in titles)
+            {
+
+            }
+        }
+
     }
 }
